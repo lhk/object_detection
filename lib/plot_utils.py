c6a02340108627de7bfc5fb4515a45163b8913a1
Lars Henning Klein <lars.klein@rwth-aachen.de>
2017-09-26 18:23:55 +0200

    fixing coordinates going over the size of the array

--- lib/plot_utils.py
+++ lib/plot_utils.py
@@ -10,32 +10,43 @@ def create_canvas(size_x=100, size_y=100, colored=False):
         return np.zeros((size_x, size_y))
 
 
-def draw_rect(np_arr, rect, color=1, thickness=0):
+def draw_rect(canvas, rect, color=1, thickness=0):
     """
     plots a rectangle into a numpy array
     coordinates of the rectangle are in [0,1]
-    :param np_arr:
+    :param canvas:
     :param rect:
     :param color:
     :param thickness:
     :return:
     """
-    size_x, size_y = np_arr.shape
+    size_x, size_y = canvas.shape[:2]
     cx, cy, dx, dy = rect
+
+    assert 0 <= cx <= 1, "coordinates must be in [0,1]"
+    assert 0 <= cy <= 1, "coordinates must be in [0,1]"
+    assert 0 <= dx <= 1, "coordinates must be in [0,1]"
+    assert 0 <= dy <= 1, "coordinates must be in [0,1]"
+
     min_x = int((cx - 0.5 * dx) * size_x)
     min_y = int((cy - 0.5 * dy) * size_y)
     max_x = int((cx + 0.5 * dx) * size_x)
     max_y = int((cy + 0.5 * dy) * size_y)
 
-    if thickness > 0:
-        rect[min_x:min_x + thickness, min_y:max_y] = color
-        rect[max_x - thickness:max_x, min_y:max_y] = color
-        rect[min_x:max_x, min_y:min_y + thickness] = color
-        rect[min_x:max_x, max_y - thickness:max_y] = color
-    else:
-        rect[min_x:max_x, min_y:max_y] = color
+    min_x = np.max(min_x, 0)
+    min_y = np.max(min_y, 0)
+    max_x = np.min(size_x - 1, max_x)
+    max_y = np.min(size_y - 1, max_y)
 
-    return rect
+    if thickness > 0:
+        canvas[min_x:min_x + thickness, min_y:max_y] = color
+        canvas[max_x - thickness:max_x, min_y:max_y] = color
+        canvas[min_x:max_x, min_y:min_y + thickness] = color
+        canvas[min_x:max_x, max_y - thickness:max_y] = color
+    else:
+        canvas[min_x:max_x, min_y:max_y] = color
+
+    return canvas
 
 
 def plot_canvas(canvas):
