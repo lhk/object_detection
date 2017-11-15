3cce60ed208a6bb28612bff160a7df9cf7cd16b7
Lars Henning Klein <lars.klein@rwth-aachen.de>
2017-11-15 23:01:33 +0100

    adding indices to the nametable, for easier lookup

--- lib/name_table.py
+++ lib/name_table.py
@@ -1,20 +1,21 @@
-names = ["aeroplane",
-"bicycle",
-"bird",
-"boat",
-"bottle",
-"bus",
-"car",
-"cat",
-"chair",
-"cow",
-"diningtable",
-"dog",
-"horse",
-"motorbike",
-"person",
-"pottedplant",
-"sheep",
-"sofa",
-"train",
-"tvmonitor"]
+names = [
+"aeroplane",    #0
+"bicycle",      #1
+"bird",         #2
+"boat",         #3
+"bottle",       #4
+"bus",          #5
+"car",          #6
+"cat",          #7
+"chair",        #8
+"cow",          #9
+"diningtable",  #10
+"dog",          #11
+"horse",        #12
+"motorbike",    #13
+"person",       #14
+"pottedplant",  #15
+"sheep",        #16
+"sofa",         #17
+"train",        #18
+"tvmonitor"]    #19
